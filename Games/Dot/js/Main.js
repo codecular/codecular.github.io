73404a75a788d1c826b9a53d8adaba7e56ea00f7
codecular <shantanu555.sp@gmail.com>
2018-04-29 22:29:24 +0530

    Dot Game Uploaded

---
+++ Games/Dot/js/Main.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	var game = new Phaser.Game(576, 1024, Phaser.AUTO);
+
+	// Add the States your game has.
+	// game.state.add("Boot", Boot);
+	// game.state.add("Menu", Menu);
+	// game.state.add("Preload", Preload);
+	
+	game.state.add("GameOver", GameOver);	
+	game.state.add("Level", Level);	
+	game.state.start("Level");
+	
+};
+
+var score=0;
+
+var message="";
